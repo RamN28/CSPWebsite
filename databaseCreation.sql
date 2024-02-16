8f07eb150a1a4150d076e58d9d1eb1255075d44c
Aditya Kolekar <128189377+HeadlessSnow786@users.noreply.github.com>
2024-02-16 06:06:43 -0800

    Added database creation script

---
+++ databaseCreation.sql
@@ -0,0 +1,22 @@
+CREATE TABLE Team (
+    id INT AUTO_INCREMENT,
+    fullname VARCHAR(255),
+    job VARCHAR(255),
+    skills VARCHAR(255),
+    bio TEXT,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO Team (fullname, job, skills, bio)
+VALUES 
+('Aditya Kolekar', 'Backend Developer', 'HTML, Python, Bootstrap, SQL, C#, Django, Game Development using Unity Engine', 'My name is Aditya Kolekar and I am currently a 11th grader at American High School. I am an avid coder who takes part in many code and robotics competitions. In my free time I enjoy playing board games, reading books, and going to the park. I want to be apart of the coming surge of people in Computer Science and AI. I want to inspire the coming generations to use AI and other tools to not only improve day to day lives, but also solve many crucial problems at the local and global scale.'),
+
+('Pranjal Mishra', 'Webmaster', 'Python, C#, Learning C++, Practical Building Skills, Troubleshooting, Unity', 'I am Pranjal Mishra, a junior at American High School. I am a sports enthusiast and avid gamer, balancing my love for badminton and video games. When not immersed in these pursuits, I find joy in the simple pleasures of playing with my dog, all while staying at the forefront of technological innovation as a dedicated tech enthusiast.'),
+
+('Aarav Mehta', 'Tech Support', 'Python, PHP, JavaScript', 'I am Aarav, I am a junior in High school and I like to play sports and play games. I find computer science fun and unique. I want to get better at coding and learn new things so I can use it for the future. I am fascinated by technology and want to help create and advance new technology as time goes on. I wish to have fun in this class and use this as a learning experience.'),
+
+('Abhinav Gondi', 'Content Specialist', 'Python, Animating with Unity Engine', 'I am Abhinav, an 11th grader at American High School. I find joy in the challenge of solving Rubiks cubes, a hobby that keeps my mind sharp and focused. When I am not twisting and turning cubes, you will likely find me on the basketball court, dribbling and shooting hoops—a passion that fuels my love for teamwork and staying active. Beyond the realm of sports, I am also fascinated by the stock market. Monitoring and analyzing market trends has become a part of my routine, and I enjoy staying informed about the ever-changing financial landscape.'),
+
+('Ram Charan Nemuri', 'Frontend Developer', 'HTML, CSS, Python', 'I am in 11th grade studying at American High School. I am currently learning how to code and use various software platforms. Outside of school, I play cricket on a team. I have started playing since 6th grade. I enjoy spending time in nature. I go biking every week. I like playing badminton, soccer with friends. I am excited to create new technology that inspires the future generations to be involved in robust platforms.'),
+
+('Rushil Mantri', 'Frontend Developer', 'Python, HTML, CSS', 'My name is Rushil Mantri and I am currently a 11th grader at American High School. I am an avid coder who takes part in many code and robotics competitions. In my free time I enjoy playing board games, reading books, and going to the park. I want to be apart of the coming surge of people in Computer Science and AI. I want to inspire the coming generations to use AI and other tools to not only improve day to day lives, but also solve many crucial problems at the local and global scale.' );
